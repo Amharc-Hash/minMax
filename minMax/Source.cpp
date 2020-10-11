be5e50eac9c2b788c3f45799604890904b8155c4
Sitruunaaaa <69064711+Sitruunaaaa@users.noreply.github.com>
2020-10-11 22:31:48 +0700

    gg1

--- minMax/Source.cpp
+++ minMax/Source.cpp
@@ -1,35 +1,49 @@
 #include<stdio.h>
-void print(double,double,double);
+void print(double, double, double);
 
 int main()
 {
-    int i, j, k;
+    int i, j, k,x=0;
     double num[10];
     double max{}, min, sum, avg;
 
     for (i = 0; i < 10; i++)
     {
         scanf_s("%lf", &num[i]);
+        if (num[i] < 0)
+        {
+            x = 1;
+            break;
+        }
     }
 
     min = num[0];
-    for (j = 0; j < 10; j++)
+    if (x==0)
     {
-        if (min > num[j] && min>=0 && max>=0)
+        for (j = 0; j < 10; j++)
         {
-            min = num[j];
+            if (min > num[j] && min >= 0 && max >= 0)
+            {
+                min = num[j];
+            }
+            else if (max < num[j] && min >= 0 && max >= 0)
+            {
+                max = num[j];
+            }
         }
-        else if (max < num[j] && min >= 0 && max >= 0)
-        {
-            max = num[j];
-        }
-    }
 
-    sum = 0;
-    for (k = 0; k < 10; k++)
-    {
-        sum += num[k];
+        sum = 0;
+        for (k = 0; k < 10; k++)
+        {
+            sum += num[k];
+        }
+        print(min, max, sum);
     }
+    else
+    {
+        printf("Undefined");
+    }
+    
     return 0;
 }
 
